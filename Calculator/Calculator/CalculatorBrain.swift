5c5294ea624d7c8cd578c0197ea46e42f0ba1a8a
Lui <injabie3@gmail.com>
2017-10-15 21:16:21 -0700

    Test Travis CI with Tailor
    Address warnings from Tailor.

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -7,49 +7,50 @@
 //
 
 import Foundation
-			
+
 
 struct CalculatorBrain {
-    
-    private var accumulator: Double? //When we first begin, we haven't set it yet, so make this an optional: 27:15
-    
+
+    private var accumulator: Double? // When we first begin, we haven't set it yet, so make this an optional: 27:15
+
     private enum Operation {
-        case constant(Double) //the (Double) specifies associated value.
+        case constant(Double) // The (Double) specifies associated value.
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double, Double) -> Double)
         case equals
     }
-    
+
     private var operations: Dictionary<String, Operation> =
     [
-        "π" : Operation.constant(Double.pi),
-        "e" : Operation.constant(M_E),
-        "√" : Operation.unaryOperation(sqrt),
-        "cos" : Operation.unaryOperation(cos),
-        "sin" : Operation.unaryOperation(sin),
-        "±" : Operation.unaryOperation({ -$0 }),
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "sin": Operation.unaryOperation(sin),
+        "±": Operation.unaryOperation{ -$0 },
         // HOLY IN LINE FUNCTIONS ARE COOL! Lecture 02: 1:10:55
         // This function can be made more simple, from this:
-        "×" : Operation.binaryOperation({ (op1: Double, op2) -> Double in return op1 * op2 }),
+        "×": Operation.binaryOperation{ (op1: Double, op2) -> Double in return op1 * op2 },
         // To this:
-        "+" : Operation.binaryOperation({ $0 + $1 }),
-        "-" : Operation.binaryOperation({ $0 - $1 }),
-        "÷" : Operation.binaryOperation({ $0 / $1 }),
-        "=" : Operation.equals
+        "+": Operation.binaryOperation{ $0 + $1 },
+        "-": Operation.binaryOperation{ $0 - $1 },
+        "÷": Operation.binaryOperation{ $0 / $1 },
+        "=": Operation.equals
     ]
-    
-    
+
+
     mutating func setOperand(_ operand: Double) {
         // We use the _ because it's clear what operand is.
         accumulator = operand
     }
-    
+
     private mutating func performPendingBinaryOperation() {
         if pendingBinaryOperation != nil && accumulator != nil {
             accumulator = pendingBinaryOperation!.perform(with: accumulator!)
             pendingBinaryOperation = nil
         }
     }
+
     private var pendingBinaryOperation: PendingBinaryOperation? // Notice how we set this as an optional, because we're not always in the middle of a binary operation! Lecture 02: 1:02:50
     
     private struct PendingBinaryOperation {
@@ -60,7 +61,7 @@ struct CalculatorBrain {
             return function(firstOperand, secondOperand)
         }
     }
-    
+
     mutating func performOperation(_ symbol: String) {
         if let operation = operations[symbol] {
             switch operation {
@@ -68,7 +69,7 @@ struct CalculatorBrain {
                 accumulator = value
             case .unaryOperation(let function):
                 if accumulator != nil {
-                    //This helps to prevent app from crashing too.
+                    // This helps to prevent app from crashing too.
                     accumulator = function(accumulator!)
                 }
             case .binaryOperation(let function):
@@ -86,7 +87,7 @@ struct CalculatorBrain {
     }
 
     var result: Double? {
-        //Make this an optional too.
+        // Make this an optional too.
         get {
             return accumulator
         }
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,41 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     var userIsInTheMiddleOfTyping = false
-    
+
     @IBOutlet weak var display: UILabel!
-    
+
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         print("\(digit) was touched")
         if userIsInTheMiddleOfTyping {
             display.text = "\(display.text!)\(digit)"
         }
-        else
-        {
+        else {
             display.text = "\(digit)"
             userIsInTheMiddleOfTyping = true
         }
-        
-        
     }
-    
-    var displayValue: Double{
-    //Computed properties in Lecture 01: 1:20:43
+
+    var displayValue: Double {
+    // Computed properties in Lecture 01: 1:20:43
         get {
-            //Must unwrap text and the Double because it's an optional.
-            //Basically every time you "get" (e.g. read) this variable it computes the value for you.
+            // Must unwrap text and the Double because it's an optional.
+            // Basically every time you "get" (e.g. read) this variable it computes the value for you.
             return Double(display.text!)!
         }
         set {
-            //Every time you "set" (e.g. assign) this variable it does the following:
+            // Every time you "set" (e.g. assign) this variable it does the following:
             display.text = String(newValue)
         }
     }
-    
-    private var brain: CalculatorBrain = CalculatorBrain() //Must initialize this because it is in a class.
-    
+
+    private var brain: CalculatorBrain = CalculatorBrain() // Must initialize this because it is in a class.
+
     @IBAction func performOperation(_ sender: UIButton) {
         if userIsInTheMiddleOfTyping {
             brain.setOperand(displayValue)
@@ -55,9 +52,8 @@ class ViewController: UIViewController {
         if let result = brain.result {
             displayValue = result
         }
-        
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -67,7 +63,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
